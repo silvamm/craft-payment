6a510d8b0753bd8d7bf9574360ef63d723c07898
silvamm <silvamm.matheus@gmail.com>
2021-12-22 11:35:57 -0300

    feat: cria metodo para buscar valor do pagamento

--- src/main/java/com/tool/craft/service/CraftService.java
+++ src/main/java/com/tool/craft/service/CraftService.java
@@ -2,13 +2,19 @@ package com.tool.craft.service;
 
 import com.tool.craft.config.CraftConfig;
 import com.tool.craft.enumm.BillType;
+import com.tool.craft.model.BillDetails;
+import com.tool.craft.model.LabelAmountGeometry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.NumberUtils;
 import software.amazon.awssdk.services.rekognition.model.*;
 
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.util.*;
+
+import static java.util.stream.Collectors.toList;
 
 @Log4j2
 @Service
@@ -17,15 +23,23 @@ public class CraftService {
     @Autowired
     private CraftConfig craftConfig;
 
-    @Autowired
-    private AwsService awsService;
+    public BillDetails findBillDetails(List<TextDetection> textDetections){
 
-    public Optional<BillType> startSearchIn(InputStream inputStream) {
-        List<TextDetection> textDetections = awsService.detectTextLabelsIn(inputStream);
-        return findBillType(textDetections);
+        Optional<BillType> optionalBillType = findBillTypeIn(textDetections);
+        Optional<String> optionalAmount = findAmountIn(textDetections);
+
+        BillDetails details = new BillDetails();
+
+        optionalBillType.ifPresent(details::setType);
+        optionalAmount.ifPresent(a -> {
+            BigDecimal amount = BigDecimal.valueOf(Double.parseDouble(a));
+            details.setAmount(amount);
+        });
+
+        return details;
     }
 
-    private Optional<BillType> findBillType(List<TextDetection> textDetections) {
+    public Optional<BillType> findBillTypeIn(List<TextDetection> textDetections) {
         for (var billsAndRecipients : craftConfig.getBills().entrySet()) {
             for (var recipient : billsAndRecipients.getValue()) {
                 if (findRecipient(textDetections, recipient)) {
@@ -36,7 +50,47 @@ public class CraftService {
         return Optional.empty();
     }
 
+    public Optional<String> findAmountIn(List<TextDetection> textDetections) {
+
+        textDetections = onlyLines(textDetections);
+
+        final LabelAmountGeometry labelAmount = new LabelAmountGeometry();
+
+        for (var textDetection : textDetections) {
+
+            if (!labelAmount.found() && (textDetection.detectedText().toLowerCase().contains("total a pagar") ||
+                    textDetection.detectedText().toLowerCase().contains("valor"))) {
+                labelAmount.set(textDetection.geometry().boundingBox().top(),
+                        textDetection.geometry().boundingBox().left());
+                continue;
+            }
+
+            if (labelAmount.found() &&
+                    labelAmount.above(
+                            textDetection.geometry().boundingBox().top(),
+                            textDetection.geometry().boundingBox().left())) {
+                String amount = onlyNumberAndDot(textDetection.detectedText());
+                return Optional.ofNullable(amount);
+            }
+
+        }
+        return Optional.empty();
+    }
+
+    private String onlyNumberAndDot(String amount){
+        amount = amount.replaceAll("[^0-9,]","");
+        amount = amount.replaceAll(",", ".");
+        return amount;
+    }
+
+    private List<TextDetection> onlyLines(List<TextDetection> textDetections){
+        return textDetections.stream()
+                .filter(t -> t.type().equals(TextTypes.LINE))
+                .collect(toList());
+    }
+
     private boolean findRecipient(List<TextDetection> textDetections, String recipient) {
+
         for (TextDetection textDetection : textDetections) {
             if (containsRecipient(textDetection, recipient)) {
                 return true;
@@ -46,8 +100,7 @@ public class CraftService {
     }
 
     private boolean containsRecipient(TextDetection textDetection, String recipient) {
-        return textDetection.type().equals(TextTypes.LINE)
-                && textDetection.detectedText().toLowerCase().contains(recipient);
+        return textDetection.detectedText().toLowerCase().contains(recipient);
     }
 
 
