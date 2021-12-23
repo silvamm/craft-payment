c3f9d5344732d001ca508eb8ce15f517b495800e
silvamm <silvamm.matheus@gmail.com>
2021-12-23 14:36:33 -0300

    feat: adiciona metodo para deletar um pagamento

--- src/main/java/com/tool/craft/controller/CraftController.java
+++ src/main/java/com/tool/craft/controller/CraftController.java
@@ -1,13 +1,13 @@
 package com.tool.craft.controller;
 
-import com.tool.craft.enumm.BillType;
 import com.tool.craft.model.BillDetails;
-import com.tool.craft.service.*;
+import com.tool.craft.service.AwsRekognitionService;
+import com.tool.craft.service.AwsS3Service;
+import com.tool.craft.service.CraftService;
+import com.tool.craft.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import software.amazon.awssdk.services.rekognition.model.TextDetection;
@@ -15,7 +15,6 @@ import software.amazon.awssdk.services.rekognition.model.TextDetection;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class CraftController {
@@ -50,21 +49,35 @@ public class CraftController {
             return modelAndView;
         }
 
-        InputStream inputStream = file.getInputStream();
+        final InputStream inputStream = file.getInputStream();
 
-        List<TextDetection> textDetections = awsRekognitionService.detectTextLabelsIn(inputStream);
-        BillDetails billDetails = craftService.findBillDetails(textDetections);
+        final List<TextDetection> textDetections = awsRekognitionService.detectTextLabelsIn(inputStream);
+        final BillDetails billDetails = craftService.findBillDetails(textDetections);
 
         if(billDetails.filled()){
-            String s3Receipt = awsS3Service.saveInBucket(inputStream);
-            paymentService.save(billDetails, s3Receipt, inputStream);
+            String s3Receipt = awsS3Service.saveInBucket(file);
+            paymentService.save(billDetails, s3Receipt);
             modelAndView.addObject("message", "Encontrado conta de " + billDetails.getType()
                     + " no valor de R$ " + billDetails.getAmount());
-            return modelAndView;
+        }else {
+            modelAndView.addObject("message",  "Detalhes da conta não encontrado");
         }
 
-        modelAndView.addObject("message",  "Detalhes da conta não encontrado");
+        modelAndView.addObject("payments",  paymentService.findAll());
         return modelAndView;
     }
+
+    @DeleteMapping("/{payment_id}")
+    public ModelAndView deletePayment(@PathVariable(name = "payment_id") Long paymentId){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        paymentService.findBy(paymentId).ifPresent(p -> {
+            awsS3Service.deleteInBucket(p.getReceipt());
+            paymentService.delete(paymentId);
+        });
+        modelAndView.addObject("payments",  paymentService.findAll());
+        return modelAndView;
+    }
+
 
 }
