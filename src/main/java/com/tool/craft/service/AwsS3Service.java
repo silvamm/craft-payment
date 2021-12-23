3c05cac1e86087f0c45846a13bde7614d2075cb4
silvamm <silvamm.matheus@gmail.com>
2021-12-23 14:37:28 -0300

    feat: adiciona metodo para deletar um objeto s3 e enviar extensao ao adicionar objeto no s3

--- src/main/java/com/tool/craft/service/AwsS3Service.java
+++ src/main/java/com/tool/craft/service/AwsS3Service.java
@@ -2,41 +2,46 @@ package com.tool.craft.service;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.utils.IoUtils;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.UUID;
+
+import static org.apache.commons.io.FilenameUtils.getExtension;
 
 @Log4j2
 @Service
 public class AwsS3Service {
 
-    public String saveInBucket(InputStream inputStream)  {
+    public String saveInBucket(MultipartFile multipartFile) throws IOException {
 
-        S3Client s3Client = S3Client.builder()
+        final S3Client s3Client = S3Client.builder()
                 .region(Region.US_EAST_1)
                 .build();
 
-        String key = UUID.randomUUID().toString();
+        String key = UUID.randomUUID() + "." + getExtension(multipartFile.getOriginalFilename());
 
-        PutObjectRequest objectRequest = PutObjectRequest.builder()
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket("craft-payment")
                 .key(key)
+                .contentLength(multipartFile.getSize())
                 .build();
 
-        byte[] bytes;
-        try {
-            bytes = IoUtils.toByteArray(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-        s3Client.putObject(objectRequest, RequestBody.fromBytes(bytes));
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(multipartFile.getBytes()));
         return key;
+    }
+
+    public void deleteInBucket(String receipt) {
+
+        final S3Client s3Client = S3Client.builder()
+                .region(Region.US_EAST_1)
+                .build();
+
+        s3Client.deleteObject(DeleteObjectRequest.builder().bucket("craft-payment").key(receipt).build());
     }
 }
