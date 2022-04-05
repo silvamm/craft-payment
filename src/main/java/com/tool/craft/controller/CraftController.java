ebc9c24fab3b42636afdb3ca7f82f2e813fcce94
Matheus Monteiro <silvamm.matheus@gmail.com>
2022-04-05 19:10:49 -0300

    refactor: utiliza as interfaces ao inves das classes concretas

--- src/main/java/com/tool/craft/controller/CraftController.java
+++ src/main/java/com/tool/craft/controller/CraftController.java
@@ -1,36 +1,33 @@
 package com.tool.craft.controller;
 
-import com.tool.craft.model.DetectedText;
-import com.tool.craft.model.entity.BillDetails;
-import com.tool.craft.service.AwsRekognitionService;
-import com.tool.craft.service.AwsS3Service;
-import com.tool.craft.service.CraftService;
-import com.tool.craft.service.PaymentService;
+import com.tool.craft.entity.BillDetails;
+import com.tool.craft.service.craft.text.Text;
+import com.tool.craft.service.craft.CraftService;
+import com.tool.craft.service.payment.PaymentService;
+import com.tool.craft.service.storage.StorageService;
+import com.tool.craft.service.ocr.TextDetectionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
-import software.amazon.awssdk.services.rekognition.model.TextDetection;
-import software.amazon.awssdk.services.rekognition.model.TextTypes;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
-import static java.util.stream.Collectors.*;
+import static org.apache.commons.io.FilenameUtils.getExtension;
 
 @Controller
 @RequiredArgsConstructor
 public class CraftController {
 
     private final CraftService craftService;
-    private final AwsS3Service awsS3Service;
-    private final AwsRekognitionService awsRekognitionService;
+    private final StorageService storageService;
+    private final TextDetectionService textDetectionService;
     private final PaymentService paymentService;
 
     @GetMapping("/")
@@ -41,7 +38,8 @@ public class CraftController {
     }
 
     @PostMapping("/start")
-    public RedirectView start(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+    public RedirectView start(@RequestParam("file") MultipartFile file,
+                              RedirectAttributes redirectAttributes) throws IOException {
 
         if (file.isEmpty()) {
             redirectAttributes.addFlashAttribute("message", "Envie um arquivo");
@@ -49,38 +47,38 @@ public class CraftController {
         }
 
         final InputStream inputStream = file.getInputStream();
-
-        final List<DetectedText> texts = awsRekognitionService
-                .detectTextLabelsIn(inputStream)
-                .stream()
-                .filter(textDetection -> textDetection.type().equals(TextTypes.LINE))
-                .map(DetectedText::new)
-                .collect(toList());
-
+        final List<Text> texts = textDetectionService.findTextsIn(inputStream);
         final Optional<BillDetails> optionalBillDetails = craftService.findBillDetailsIn(texts);
 
         optionalBillDetails
                 .ifPresentOrElse(billDetails -> {
-                    String s3Receipt = null;
+                    String receipt = null;
                     try {
-                        s3Receipt = awsS3Service.saveInBucket(file);
+                        receipt = storageService.save(file.getBytes(),
+                                file.getSize(),
+                                getExtension(file.getOriginalFilename()));
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    paymentService.save(billDetails, s3Receipt);
-                    redirectAttributes.addFlashAttribute("message", "Encontrado conta de " + billDetails.getType()
-                            + " no valor de R$ " + billDetails.getAmount());
+                    paymentService.save(billDetails, receipt);
+
+                    redirectAttributes.addFlashAttribute("message",
+                            String.format("Encontrado conta de %s no valor de R$ %s",
+                                    billDetails.getType(), billDetails.getAmount()));
                 },
-                () -> redirectAttributes.addFlashAttribute("message", "Detalhes da conta não encontrado"));
+                () -> redirectAttributes.addFlashAttribute("message",
+                        "Detalhes da conta não encontrado"));
 
         return new RedirectView("/");
     }
 
     @DeleteMapping("/{payment_id}")
-    public RedirectView deletePayment(@PathVariable(name = "payment_id") Long paymentId){
-        paymentService.findBy(paymentId).ifPresent(p -> {
-            awsS3Service.deleteInBucket(p.getReceipt());
+    public RedirectView deletePayment(@PathVariable(name = "payment_id") Long paymentId,
+                                      RedirectAttributes redirectAttributes){
+        paymentService.findBy(paymentId).ifPresent(payment -> {
+            storageService.delete(payment.getReceipt());
             paymentService.delete(paymentId);
+            redirectAttributes.addFlashAttribute("message", "Arquivo excluido com sucesso");
         });
         return new RedirectView("/");
     }
