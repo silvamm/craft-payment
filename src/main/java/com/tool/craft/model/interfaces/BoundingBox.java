669e15d6830f0810a148297432caff4a2ed55c3f
Matheus Monteiro <silvamm.matheus@gmail.com>
2022-04-05 12:41:25 -0300

    feat: adiciona interfaces para processo de OCR

---
+++ src/main/java/com/tool/craft/model/interfaces/BoundingBox.java
@@ -0,0 +1,7 @@
+package com.tool.craft.model.interfaces;
+
+public interface BoundingBox {
+
+    Float getTop();
+    Float getLeft();
+}
---
+++ src/main/java/com/tool/craft/model/interfaces/Text.java
@@ -0,0 +1,7 @@
+package com.tool.craft.model.interfaces;
+
+public interface Text {
+    String get();
+
+    boolean contains(String text);
+}
