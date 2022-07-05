83f8d5542f31b6624cc4a13b10605ef95b18fe66
Matheus Monteiro <silvamm.matheus@gmail.com>
2022-07-04 22:02:45 -0300

    feat: implementa metodo para buscar arquivo

--- src/main/java/com/tool/craft/service/storage/StorageService.java
+++ src/main/java/com/tool/craft/service/storage/StorageService.java
@@ -2,7 +2,11 @@ package com.tool.craft.service.storage;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+
 public interface StorageService {
+
+    InputStream get(String key);
 
     String save(MultipartFile file);
 
