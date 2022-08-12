498c4b38fd6a28e2ba7dfe89ff82ed8d8a16a057
Matheus Monteiro <silvamm.matheus@gmail.com>
2022-08-12 00:33:04 -0300

    refactor: renomei classe

--- src/main/java/com/tool/craft/dto/Ec2InfoLambdaDto.java
+++ src/main/java/com/tool/craft/dto/Ec2InfoLambdaDto.java
@@ -3,7 +3,7 @@ package com.tool.craft.dto;
 import lombok.Data;
 
 @Data
-public class Ec2UrlLambdaDto {
+public class Ec2InfoLambdaDto {
 
     private String keyName;
     private Ec2StateDto state;
