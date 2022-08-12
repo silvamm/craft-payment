059ac5d2f57a39a7363d6cf583650695ca465eba
Matheus Monteiro <silvamm.matheus@gmail.com>
2022-08-12 00:34:00 -0300

    feat: adiciona chamada ao lambda para pegar informacoes do banco de dados

--- src/main/java/com/tool/craft/config/DataSourceConfig.java
+++ src/main/java/com/tool/craft/config/DataSourceConfig.java
@@ -1,7 +1,7 @@
 package com.tool.craft.config;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import com.tool.craft.dto.Ec2UrlLambdaDto;
+import com.tool.craft.dto.Ec2InfoLambdaDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,21 +19,29 @@ public class DataSourceConfig {
     private String user;
     @Value("${spring.datasource.password}")
     private String password;
-    @Value("${app.aws.lambda.url}")
+    @Value("${app.aws.ec2.lambda.url}")
     private String awsLambdaUrl;
+    @Value("${app.aws.ec2.database.id}")
+    private String databaseInstanceId;
 
     @Bean
     public DataSource getDataSource() {
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Ec2UrlLambdaDto> response =
-                restTemplate.exchange(awsLambdaUrl,
+        ResponseEntity<Ec2InfoLambdaDto> response =
+                restTemplate.exchange(awsLambdaUrl + "/" + databaseInstanceId,
                         HttpMethod.GET,
                         null,
-                        Ec2UrlLambdaDto.class);
+                        Ec2InfoLambdaDto.class);
+
+        Ec2InfoLambdaDto ec2Info = response.getBody();
+        if(ec2Info == null)
+            throw new IllegalStateException("Não foi possivel obter informações do banco de dados");
+        if(ec2Info.getPublicDnsName() == null)
+            throw new IllegalStateException("Banco de dados indisponivel");
 
         MysqlDataSource mysqlDataSource = new MysqlDataSource();
-        mysqlDataSource.setURL("jdbc:mysql://"+response.getBody().getPublicDnsName()+"/craft_db");
+        mysqlDataSource.setURL("jdbc:mysql://"+ec2Info.getPublicDnsName()+"/craft_db");
         mysqlDataSource.setUser(user);
         mysqlDataSource.setPassword(password);
         return mysqlDataSource;
