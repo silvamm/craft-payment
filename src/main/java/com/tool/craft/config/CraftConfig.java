9e95e9e540d3d81918fb78dcc7d1519582ea94f5
Matheus Monteiro <silvamm.matheus@gmail.com>
2022-12-03 04:15:22 -0300

    refactor: altera nome da propriedade

--- src/main/java/com/tool/craft/config/CraftConfig.java
+++ src/main/java/com/tool/craft/config/CraftConfig.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class CraftConfig {
 
     private Map<BillType, List<String>> bills;
-    private List<String> labelTargets;
+    private List<String> targetLabels;
+
 
 }
